1126a9d85010069537ac8dc142eaafed10d34f26
Tavis Ormandy <taviso@gmail.com>
2015-07-27 22:03:19 -0700

    print out something if enable fails

--- ctypes.sh
+++ ctypes.sh
@@ -12,7 +12,9 @@ function initialize_ctypes_module()
 
     enable -f ctypes.so ${builtins[@]} &> /dev/null || {
         # is it possible user doesn't have /usr/local/lib in library search path?
-        enable -f /usr/local/lib/ctypes.so ${builtins[@]}
+        enable -f /usr/local/lib/ctypes.so ${builtins[@]} || {
+            echo "can't find the ctypes.so library, run make install?" 1>&2
+        }
     }
 }
 
