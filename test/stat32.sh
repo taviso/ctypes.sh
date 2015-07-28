1a8b8dbd424db535801c682a36da90babcf356f9
Tavis Ormandy <taviso@gmail.com>
2015-07-28 11:21:05 -0700

    fix test script for 32-bit stat

--- test/stat32.sh
+++ test/stat32.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-
 source ../ctypes.sh
 
 # Allocate some space for the stat buffer
-dlcall -n statbuf -r pointer $RTLD_DEFAULT malloc 1024
+dlcall -n statbuf -r pointer $RTLD_DEFAULT calloc 1024 1
 
 # Define the format of struct stat for bash
 declare -a stat
 {
     unset n
     stat[st_dev     = n++]="longlong"
-    stat[             n++]="short"  # Padding
-    stat[st_ino     = n++]="long"
-    stat[st_mode    = n++]="int"
-    stat[st_nlink   = n++]="int"
-    stat[st_uid     = n++]="int"
-    stat[st_gid     = n++]="int"
-    stat[             n++]="short"  # Padding
+    stat[             n++]="unsigned"  # Padding
+    stat[st_ino     = n++]="unsigned"
+    stat[st_mode    = n++]="unsigned"
+    stat[st_nlink   = n++]="unsigned"
+    stat[st_uid     = n++]="unsigned"
+    stat[st_gid     = n++]="unsigned"
+    stat[             n++]="unsigned"  # Padding
     stat[st_rdev    = n++]="longlong"
-    stat[st_size    = n++]="long"
-    stat[st_blksize = n++]="long"
-    stat[st_blocks  = n++]="long"
+    stat[st_size    = n++]="unsigned"
+    stat[st_blksize = n++]="unsigned"
+    stat[st_blocks  = n++]="unsigned"
 }
 
 # stat is not exported, use xstat instead.
-dlcall $RTLD_DEFAULT __xstat 0 "/etc/passwd" $statbuf
+dlcall $RTLD_DEFAULT __xstat 3 "/etc/passwd" $statbuf
 unpack $statbuf stat
 
 printf "/etc/passwd\n"
@@ -32,6 +31,8 @@ printf "\tuid:  %u\n" ${stat[st_uid]##*:}
 printf "\tgid:  %u\n" ${stat[st_gid]##*:}
 printf "\tmode: %o\n" ${stat[st_mode]##*:}
 printf "\tsize: %u\n" ${stat[st_size]##*:}
+
+printf "%#x\n" ${stat[@]##*:}
 
 dlcall $RTLD_DEFAULT free $statbuf
 
