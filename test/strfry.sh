7b8fb0197c3a3b3f70db96c053313ecc8e23826d
Conrad Meyer <cse.cem@gmail.com>
2015-08-05 14:24:43 -0700

    test/strfry.sh: Port to not-Linux (anything with memmove(3))

--- test/strfry.sh
+++ test/strfry.sh
@@ -3,20 +3,24 @@
 source ../ctypes.sh
 set -x
 
-dlopen libc.so.6
-
-dlcall -r pointer ${DLHANDLES[libc.so.6]} strdup "hello, world"
+dlcall -n hwstr -r pointer $RTLD_DEFAULT strdup "hello, world"
 
 # Check that the string was duplicated
-if test "$(dlcall -r int ${DLHANDLES[libc.so.6]} puts $DLRETVAL)" != "hello, world"; then
+if test "$(dlcall -r int $RTLD_DEFAULT puts $hwstr)" != "hello, world"; then
     echo FAIL
+    exit 1
 fi
 
-# Check that we can modify it
-dlcall -r pointer ${DLHANDLES[libc.so.6]} strfry "hello, world"
+if [ "$(uname -s)" = "Linux" ]; then
+    # Check that we can modify it
+    dlcall -r pointer $RTLD_DEFAULT strfry $hwstr
+else
+    dlcall -r pointer $RTLD_DEFAULT memmove $hwstr "a" ulong:1
+fi
 
-if test "$(dlcall -r int ${DLHANDLES[libc.so.6]} puts $DLRETVAL)" == "hello, world"; then
+if test "$(dlcall -r int $RTLD_DEFAULT puts $DLRETVAL)" == "hello, world"; then
     echo FAIL
+    exit 1
 fi
 
 echo PASS
