b862cd0d1753cb0bcab35088b672a5ce3bf74c4b
Conrad Meyer <cse.cem@gmail.com>
2015-08-14 15:50:56 -0700

    Issue 13 - Add raw-ish hex float mode
    On the 'a' format from C99:
    
    """
    The double argument is rounded and converted to hexadecimal notation in
    the style [-]0xh.hhhp[±]d...  If the precision is missing, it is taken
    as enough to represent the floating-point number exactly, and no
    rounding occurs.
    """
    
    Tested math.sh on Linux (Fedora 22) and FreeBSD CURRENT-ish.

--- test/math.sh
+++ test/math.sh
@@ -9,17 +9,20 @@ dlopen libm.so
 
 function verify_result()
 {
-    dlcall -n result -r double ${DLHANDLES[libm.so]} ${1} ${2}
+    dlcall -n result -r ${3} ${DLHANDLES[libm.so]} ${1} ${2}
 
-    if test "$result" != "$3"; then
+    if test "$result" != "${3}:${4}"; then
         echo FAIL
         exit 1
     fi
 }
 
-verify_result sin double:0 double:0.000000
-verify_result sin double:-0 double:-0.000000
-verify_result sin double:0.7 double:0.644218
-verify_result sin double:1.57079632679489661923 double:1.000000 # pi/2
+verify_result sin double:0 double 0.000000
+verify_result sin double:-0 double -0.000000
+verify_result sin double:0.7 double 0.644218
+verify_result sin double:1.57079632679489661923 double 1.000000 # pi/2
+
+verify_result sin rawdouble:0x1.921fb54442d18p+0 rawdouble 0x1p+0 # pi/2
+verify_result sin rawdouble:-0x0p+0 rawdouble -0x0p+0
 
 echo PASS
