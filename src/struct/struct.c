8f47374297c61c60d5147b7ab7c34b5656ed6a28
Tavis Ormandy <taviso@gmail.com>
2016-06-25 10:44:37 -0700

    first attempt at creating the struct builtin

--- src/struct/struct.c
+++ src/struct/struct.c
@@ -20,15 +20,24 @@
 #include "types.h"
 #include "shell.h"
 
-// We don't use the libctf stuff, silence loader's missing symbol complaints.
-struct debug_fmt_ops ctf__ops;
 
+// This is just to disable ctf support.
+static int debug_fmt_error(void) { return -1; }
+
+// Export an unused debug format to disable ctf.
+struct debug_fmt_ops ctf__ops = {
+    .load_file = debug_fmt_error,
+};
+
+// Used to store context inside dwarves callbacks.
 struct cookie {
-    char        *typename;
-    int          result;
-    HASH_TABLE  *assoc;
-    char       **filenames;
-    unsigned     nfiles;
+    char             *typename;
+    int               result;
+    char            **filenames;
+    unsigned          nfiles;
+    SHELL_VAR        *assoc;
+    struct cus       *cus;
+    struct conf_load *conf;
 };
 
 // This gets called once for every compilation unit, and we're expected to
@@ -73,29 +82,36 @@ static enum load_steal_kind create_array_stealer(struct cu *cu, struct conf_load
 
         // If this is a base type, we're done and can set this member.
         if (type->tag == DW_TAG_base_type) {
-            char buf[64] = {0};
+            char value[64] = {0};
+            char varname[128] = {0};
 
-            tag__name(type, cu, buf, sizeof(buf), &conf);
+            tag__name(type, cu, value, sizeof value, &conf);
 
             fprintf(stderr, "struct: \t->base %s is a %s\n",
                             class_member__name(member, cu),
-                            buf);
+                            value);
+
+            snprintf(varname, sizeof varname, "%s[\"%s\"]", cookie->assoc->name, class_member__name(member, cu));
+
+            if (assign_array_element(varname, value, AV_USEIND) == NULL) {
+                fprintf(stderr, "struct: error setting %s!\n", varname);
+            }
         }
 
         // TODO
         if (tag__is_struct(type) || tag__is_union(type) || tag__is_enumeration(type)) {
             fprintf(stderr, "struct: sorry, this isn't supported yet, soon!\n");
-            return LSK__DELETE;
+            return LSK__STOP_LOADING;
         }
 
         if (type->tag == DW_TAG_array_type) {
             fprintf(stderr, "struct: sorry, this isn't supported yet, soon!\n");
-            return LSK__DELETE;
+            return LSK__STOP_LOADING;
         }
     }
 
     // If we reach here, we were able to successfully export the struct.
-    cookie->result = EXIT_SUCCESS;
+    cookie->result = EXECUTION_SUCCESS;
 
     // No need to keep loading.
     return LSK__STOP_LOADING;
@@ -105,43 +121,68 @@ static int shared_library_callback(struct dl_phdr_info *info, size_t size, void 
 {
     struct cookie *config = data;
 
+    if (strlen(info->dlpi_name) == 0)
+        return 0;
+
     fprintf(stderr, "struct: learned about object %s\n", info->dlpi_name);
+
+    cus__load_file(config->cus, config->conf, info->dlpi_name);
+
+    if (config->result == EXECUTION_SUCCESS) {
+        fprintf(stderr, "struct: found a match for symbol after %s\n", info->dlpi_name);
+        return 1;
+    }
 
     return 0;
 }
 
 static int generate_standard_struct(WORD_LIST *list)
 {
-    struct cus *cus = cus__new();
-    struct cookie config = {
-        .typename   = "stat",
-        .result     = EXIT_FAILURE,
-        .assoc      = NULL,
-        .filenames  = NULL,
-        .nfiles     = 0,
-    };
     struct conf_load conf_load = {
         .steal                  = create_array_stealer,
-        .cookie                 = &config,
         .format_path            = NULL,
         .extra_dbg_info         = false,
         .fixup_silly_bitfields  = true,
         .get_addr_info          = false,
     };
+    struct cookie config = {
+        .result     = EXECUTION_FAILURE,
+        .assoc      = NULL,
+        .cus        = cus__new(),
+        .conf       = &conf_load,
+    };
 
-    config.filenames = calloc(2, sizeof(char *));
-    config.nfiles++;
+    // Verify we have two parameters.
+    if (!list || !list->next) {
+        builtin_usage();
+        return EXECUTION_FAILURE;
+    }
 
-    // Temporary, while I get .gnu_debuginfo support working.
-    config.filenames[0] = "/usr/lib/debug/lib/x86_64-linux-gnu/libc-2.19.so",
+    // Create the array used to save the result.
+    config.assoc     = make_new_assoc_variable(list->next->word->word);
+    config.typename  = list->word->word;
+    conf_load.cookie = &config;
 
-    dl_iterate_phdr(shared_library_callback, &config);
+    // Throw away the default hash table.
+    assoc_dispose((void *) config.assoc->value);
+
+    // Replace it with our own hash table with just one bucket.
+    config.assoc->value = (char *) assoc_create(1);
 
     dwarves__init(0);
 
-    cus__load_files(cus, &conf_load, config.filenames);
+    dl_iterate_phdr(shared_library_callback, &config);
 
-    cus__delete(cus);
+    if (config.result != EXECUTION_SUCCESS) {
+        fprintf(stderr, "struct: struct not found or couldnt be parsed, may be incomplete\n");
+    }
+
+    fprintf(stderr, "struct: here is what I generated:\n\t%s=%s\n",
+                    list->next->word->word,
+                    assoc_to_string(config.assoc->value, ",", true));
+
+
+    cus__delete(config.cus);
     dwarves__exit();
     return config.result;
 }
