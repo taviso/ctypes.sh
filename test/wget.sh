ffecb149de7a2da2d6c93c7946dbcb4d51562616
Tavis Ormandy <taviso@gmail.com>
2016-07-06 20:09:28 -0700

    make testcase print PASS if output looks correct

--- test/wget.sh
+++ test/wget.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+#
+# Simple getaddrinfo() example.
+#
 
 source ../ctypes.sh
 
 declare -r AF_UNSPEC=int:0
 declare -r SOCK_STREAM=int:1
 
-struct addrinfo hints;
-struct addrinfo result;
+struct addrinfo hints
+struct addrinfo result
 
-# This is used to dereference a pointer
+# getaddrinfo requires a struct getaddrinfo **, stored in this array.
 declare -a nativeptr=(pointer)
 
 # Request to send
@@ -18,8 +21,6 @@ declare request=$'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
 
 hints[ai_family]=$AF_UNSPEC
 hints[ai_socktype]=$SOCK_STREAM
-hints[ai_flags]=int:0
-hints[ai_protocol]=int:0
 
 # Allocate space for a native structure.
 dlcall -r pointer -n hintsptr malloc $(sizeof addrinfo)
@@ -43,7 +44,7 @@ if [[ $s != int:0 ]]; then
     exit 1
 fi
 
-# Translate that result into bash structure.
+# Translate the result into bash structure.
 unpack $resultptr nativeptr
 unpack $nativeptr result
 
@@ -53,24 +54,21 @@ while true; do
     dlcall -r int -n sfd socket ${result[ai_family]} ${result[ai_socktype]} ${result[ai_protocol]}
     dlcall -r int -n ret connect $sfd ${result[ai_addr]} ${result[ai_addrlen]}
 
+    # Check if connect() succeeded
     if [[ $ret == int:0 ]]; then
-        echo successfully connected to ${result[ai_addr]}...
         break
     fi
 
     # close unused socket
     dlcall close $sfd
 
-    echo connect to ${result[ai_addr]} failed, trying ${result[ai_next]}...
-
-    # Check if this is the last result
+    # Check if there is another address to try
     if [[ ${result[ai_next]} == $NULL ]]; then
         break
     fi
 
     # Move to the next element of list
     unpack ${result[ai_next]} result
-    set | grep ^result=
 done
 
 dlcall freeaddrinfo $nativeptr
@@ -82,20 +80,23 @@ if [[ $ret != int:0 ]]; then
     exit 1
 fi
 
-# Send a GET request and read 1024 bytes of response.
-dlcall -r pointer -n buf calloc 1024 1
+# Send a GET request and read a few bytes of response.
+dlcall -r pointer -n buf calloc 128 1
 dlcall -r int -n ret write $sfd string:"${request}" ${#request}
-dlcall -r int -n ret read $sfd $buf 1023
+dlcall -r int -n ret read $sfd $buf 128
 
 dlcall close $sfd
 
 # Check if that worked
 if [[ $ret != int:-1 ]]; then
     dlcall puts $buf
-    printf "\n<result truncated>\n"
-    dlcall free $buf
-    exit 0
+
+    # Check if that returned something sane
+    dlcall -r int -n result strncmp $buf "HTTP/1.1 200 OK"  15
+    if [[ $result == int:0 ]]; then
+        echo PASS
+    fi
 fi
 
 dlcall free $buf
-exit 1
+exit 0
