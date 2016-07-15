b3e8e69c44b31bb5d71fcb5fc3afb063ed816cde
Tavis Ormandy <taviso@gmail.com>
2016-07-15 11:47:47 -0700

    cleanup stat testcase

--- test/newstat.sh
+++ test/newstat.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-source ../ctypes.sh
+source ctypes.sh
 
-# Allocate some space for the stat buffer
-dlcall -n statbuf -r pointer malloc $(sizeof stat)
+# exit on error
+set -e
 
 # Define the format of struct stat for bash
 struct stat passwd
 
-# stat is not exported, use xstat instead.
-dlcall -r int __xstat 0 "/etc/passwd" $statbuf
+# Allocate some space for the stat buffer
+sizeof -m statbuf stat
 
+# stat is not exported on Linux, use xstat instead.
+if test "$(uname)" == "Linux"; then
+    dlcall -r int __xstat 0 "/etc/passwd" $statbuf
+else
+    dlcall -r int stat "/etc/passwd" $statbuf
+fi
+
+# Convert result into bash structure
 unpack $statbuf passwd
 
 printf "/etc/passwd\n"
@@ -21,6 +29,7 @@ printf "\tsize: %u\n" ${passwd[st_size]##*:}
 
 dlcall free $statbuf
 
+# Check result is correct
 if test ${passwd[st_size]##*:} -eq $(wc -c < /etc/passwd); then
     echo PASS
     exit 0
